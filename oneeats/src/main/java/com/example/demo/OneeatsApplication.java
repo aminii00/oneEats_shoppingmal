a0c68bf2f46c6c6cfcfa34d4451fd2cfdb9fcf92
Administrator <Administrator@User -2023HMSSV>
2023-08-01 17:26:22 +0900

    hjkhjkhkjhkjhkj

--- oneeats/src/main/java/com/example/demo/OneeatsApplication.java
+++ oneeats/src/main/java/com/example/demo/OneeatsApplication.java
@@ -8,7 +8,7 @@ public class OneeatsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OneeatsApplication.class, args);
-		System.out.println("asdfsdaf");
+		System.out.println("asdf");
 	}
 
 }
